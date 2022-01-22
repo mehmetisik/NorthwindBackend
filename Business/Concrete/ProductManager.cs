73761d8f3b72919ae35f570cf71be0b7476a269f
Mehmet IŞIK <mehmetISIK1989@hotmail.com>
2022-01-22 17:33:20 +0300

    Add project files.

---
+++ Business/Concrete/ProductManager.cs
@@ -0,0 +1,56 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class ProductManager : IProductService
+    {
+        private IProductDAL _productDal;
+
+        public ProductManager(IProductDAL productDal)
+        {
+            _productDal = productDal;
+        }
+
+        public void Add(Product product)
+        {
+
+            //business codes
+            _productDal.Add(product);   
+
+
+        }
+
+        public void Delete(Product product)
+        {
+           _productDal.Delete(product);
+        }
+
+        public Product GetById(int productId)
+        {
+            return _productDal.Get(fiter: p => p.ProductId == productId);
+        }
+
+        public List<Product> GetList()
+        {
+            return _productDal.GetList().ToList();
+        }
+
+        public List<Product> GetListByCategory(int categoryId)
+        {
+            return _productDal.GetList(filter: p=> p.CategoryId == categoryId).ToList();
+        }
+
+        public void Update(Product product)
+        {
+            _productDal.Update(product);
+        }
+    }
+}
